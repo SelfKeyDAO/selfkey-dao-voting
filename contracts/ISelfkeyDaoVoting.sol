1a112824c4193448e7c62aa92e06785f98442c3e
Andre Goncalves <andre@andregoncalves.com>
2024-02-12 12:35:59 +0000

    CHORE: linting fixes

--- contracts/ISelfkeyDaoVoting.sol
+++ contracts/ISelfkeyDaoVoting.sol
@@ -12,27 +12,16 @@ interface ISelfkeyDaoVoting {
     }
 
     event AuthorizationContractAddressChanged(address indexed _address);
-
     event ProposalCreated(uint256 proposalId, string title, bool active);
-
     event ProposalChanged(uint256 proposalId, string title, bool active);
-
     event VoteCast(uint256 indexed proposalId, address indexed voter, uint256 votes);
 
     function initialize(address _authorizationContract) external;
-
     function setAuthorizationContractAddress(address _newAuthorizationContractAddress) external;
-
     function createProposal(string memory _title, bool _active) external;
-
     function updateProposal(uint256 _proposalId, string memory _title, bool _active) external;
-
     function vote(address _voter, uint256 _votes, bytes32 _param, uint _timestamp, address _signer, bytes memory signature) external;
-
     function getVoteCount(uint256 _proposalId) external view returns (uint256);
-
     function hasUserVoted(uint256 _proposalId, address _voter) external view returns (bool);
-
     function numProposals() external view returns (uint256);
-
 }
